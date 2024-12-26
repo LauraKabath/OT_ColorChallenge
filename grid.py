16b1304e5d16b24417bd782fa4b33e3abf93c67b
LauraKabath <lara.kabath@gmail.com>
2024-12-26 21:33:45 +0100

    grid.py remove_blocks
    + implementation of this method in main.py, mouse-event

--- grid.py
+++ grid.py
@@ -3,12 +3,13 @@ import random
 
 
 class Grid:
-    def __init__(self, screen, size, cell_size):
+    def __init__(self, screen, cell_count, cell_size):
         self.__screen = screen
-        self.__size = size
+        self.__cell_count = cell_count
         self.__cell_size = cell_size
-        self.__grid = [[random.randint(1, 4) for i in range(self.__size)] for j in range(self.__size)]
+        self.__grid = [[random.randint(1, 4) for i in range(self.__cell_count)] for j in range(self.__cell_count)]
         self.__selection = set()
+        self.__size = self.__cell_count * self.__cell_size
         self.__COLORS = {
             0: (200, 200, 200),  # pozadie siva
             1: (255, 186, 0),  # zlta
@@ -25,8 +26,8 @@ class Grid:
         }
 
     def draw_grid(self):
-        for row in range(self.__size):
-            for col in range(self.__size):
+        for row in range(self.__cell_count):
+            for col in range(self.__cell_count):
                 color = self.__COLORS[self.__grid[row][col]]
                 pg.draw.rect(self.__screen, color,
                              (col * self.__cell_size, row * self.__cell_size, self.__cell_size, self.__cell_size))
@@ -34,7 +35,7 @@ class Grid:
                              (col * self.__cell_size, row * self.__cell_size, self.__cell_size, self.__cell_size), 1)
 
     def select_block(self, row, col, color):
-        if (row, col) in self.__selection or row < 0 or col < 0 or row >= self.__size or col >= self:
+        if (row, col) in self.__selection or row < 0 or col < 0 or row >= self.__cell_count or col >= self.__cell_count:
             return
         if self.__grid[row][col] != color:
             return
@@ -48,6 +49,27 @@ class Grid:
         if len(self.__selection) == 1:
             self.__selection.pop()
 
+    def remove_blocks(self):
+        for row, col in self.__selection:
+            self.__grid[row][col] = 0
+
+        for col in range(self.__cell_count):
+            column = [self.__grid[row][col] for row in range(self.__cell_count)]
+            column = [i for i in column if i != 0]
+            column = [0] * (self.__cell_count - len(column)) + column
+            for row in range(self.__cell_count):
+                self.__grid[row][col] = column[row]
+
+        for col in range(self.__cell_count - 1, -1, -1):
+            if all([self.__grid[row][col] == 0 for row in range(self.__cell_count)]):
+                for shift_col in range(col, self.__cell_count - 1):
+                    for row in range(self.__cell_count):
+                        self.__grid[row][shift_col] = self.__grid[row][shift_col + 1]
+                for row in range(self.__cell_count):
+                    self.__grid[row][self.__cell_count - 1] = 0
+
+        self.clear_selection()
+
     def highlight_selection(self):
         for row, col in self.__selection:
             color = self.__HIGHLIGHT_COLORS[self.__grid[row][col]]
@@ -59,6 +81,9 @@ class Grid:
 
     def get_selection(self):
         return self.__selection
+
+    def get_size(self):
+        return self.__size
 
     def clear_selection(self):
         self.__selection.clear()
--- main.py
+++ main.py
@@ -8,25 +8,37 @@ WIDTH = 800
 HEIGHT = 500
 
 CELL_SIZE = 50
-GRID_SIZE = 10
+CELL_COUNT = 10
 
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Color Challenge")
 clock = pg.time.Clock()
 
-game = Grid(screen, GRID_SIZE, CELL_SIZE)
+game = Grid(screen, CELL_COUNT, CELL_SIZE)
 
 game_font = pg.font.Font("freesansbold.ttf", 30)
 
 while True:
     screen.fill((255, 255, 255))
     game.draw_grid()
-    x,y = pg.mouse.get_pos()
+    game.highlight_selection()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                x, y = pg.mouse.get_pos()
+                col = x // CELL_SIZE
+                row = y // CELL_SIZE
+                if 0 <= x < game.get_size() and 0 <= y < game.get_size():
+                    if game.get_cell(row, col) != 0:
+                        if (row, col) in game.get_selection():
+                            game.remove_blocks()
+                        else:
+                            game.clear_selection()
+                            game.select_block(row, col, game.get_cell(row, col))
 
     pg.display.update()
     clock.tick(60)
