e4bf65aee79d102caed15a016bdcbc866d42b269
LauraKabath <lara.kabath@gmail.com>
2024-12-28 14:24:06 +0100

    main.py class Game def run, update

--- main.py
+++ main.py
@@ -3,46 +3,58 @@ import sys
 
 from grid import Grid
 
-pg.init()
 WIDTH = 800
 HEIGHT = 500
 
 CELL_SIZE = 50
 GRID_SIZE = 10
+BLACK = (0, 0, 0)
 
 
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Color Challenge")
-clock = pg.time.Clock()
-black = (0, 0, 0)
-game = Grid(screen, GRID_SIZE, CELL_SIZE)
+class Game:
+    def __init__(self):
+        pg.init()
+        self.__screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Color Challenge")
+        self.__clock = pg.time.Clock()
+        self.__grid = Grid(self.__screen, GRID_SIZE, CELL_SIZE)
+        self.__running = True
+        self.__font = pg.font.Font("freesansbold.ttf", 30)
+        self.__remaining_cells = self.__font.render(f"Remaining: {self.__grid.get_remaining_cells()}", False, BLACK)
+        self.__selected_cells = self.__font.render(f"Selected: {len(self.__grid.get_selection())}", False, BLACK)
 
-game_font = pg.font.Font("freesansbold.ttf", 30)
+    def update(self):
+        self.__remaining_cells = self.__font.render(f"Remaining: {self.__grid.get_remaining_cells()}", False, BLACK)
+        self.__selected_cells = self.__font.render(f"Selected: {len(self.__grid.get_selection())}", False, BLACK)
+        self.__screen.blit(self.__remaining_cells, (520, 250))
+        self.__screen.blit(self.__selected_cells, (520, 220))
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                x, y = pg.mouse.get_pos()
-                col = x // CELL_SIZE
-                row = y // CELL_SIZE
-                if 0 <= x < game.get_full_size() and 0 <= y < game.get_full_size():
-                    if game.get_cell(row, col) != 0:
-                        if (row, col) in game.get_selection():
-                            game.remove_blocks()
-                        else:
-                            game.clear_selection()
-                            game.select_block(row, col, game.get_cell(row, col))
+    def run(self):
+        while self.__running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        x, y = pg.mouse.get_pos()
+                        col = x // CELL_SIZE
+                        row = y // CELL_SIZE
+                        if 0 <= x < self.__grid.get_full_size() and 0 <= y < self.__grid.get_full_size():
+                            if self.__grid.get_cell(row, col) != 0:
+                                if (row, col) in self.__grid.get_selection():
+                                    self.__grid.remove_blocks()
+                                else:
+                                    self.__grid.clear_selection()
+                                    self.__grid.select_block(row, col, self.__grid.get_cell(row, col))
+            self.__screen.fill((255, 255, 255))
+            self.__grid.draw_grid()
+            self.__grid.highlight_selection()
+            self.update()
+            pg.display.update()
+            self.__clock.tick(60)
 
-    screen.fill((255, 255, 255))
-    game.draw_grid()
-    game.highlight_selection()
-    remaining_cells = game_font.render(f"Remaining: {game.get_remaining_cells()}", False, black)
-    screen.blit(remaining_cells, (520, 250))
-    selected_cells = game_font.render(f"Selected: {len(game.get_selection())}", False, black)
-    screen.blit(selected_cells, (520, 220))
-    pg.display.update()
-    clock.tick(60)
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
